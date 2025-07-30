7b9bd1e894ee65cc153943de8621d69ed9ff25cf
kntWT <kento.wata0628@gmail.com>
2025-07-30 20:53:31 +0900

    fixup! chore(deps/api): update api schema

--- packages/neck-pose-estimator/src/test/fixture/user.ts
+++ packages/neck-pose-estimator/src/test/fixture/user.ts
@@ -6,20 +6,24 @@ const calibrated: components["schemas"]["User"] = {
   password: "testpassword",
   token: "testtoken",
   email: null,
+  iconUrl: null,
   standardPostureId: 1,
   isAdmin: false,
   createdAt: new Date("2025/07/26").toISOString(),
+  updatedAt: new Date("2025/07/26").toISOString(),
 };
 
 const notCalibrated: components["schemas"]["User"] = {
   id: 2,
   name: "Uncalibrated User",
   password: "testpassword",
-  token: "testtoken",
   email: null,
+  iconUrl: null,
+  token: "testtoken",
   standardPostureId: null,
   isAdmin: false,
   createdAt: new Date("2025/07/26").toISOString(),
+  updatedAt: new Date("2025/07/26").toISOString(),
 };
 
 export const userFixture = {
